c21d2edb106817940034c67dd33d9661ebad02a5
piqui81 <piqui81@0f29aa86-dbb6-f330-50e9-323f6ceba81d>
2012-03-14 14:25:18 +0000

    incluye los 3 primeros con el orden y el limite

--- trunk/madera/dbindex.php
+++ trunk/madera/dbindex.php
@@ -1,7 +1,7 @@
 <?php
 include 'db.php';
 
-if ($res = $conexion->query("SELECT i.*, f.`foto` AS `imagen` FROM `md_item` i INNER JOIN `md_foto` f ON f.`id_item` = i.`id` WHERE f.`preferida` = '1'")) {
+if ($res = $conexion->query("SELECT i.*, f.`foto` AS `imagen` FROM `md_item` i INNER JOIN `md_foto` f ON f.`id_item` = i.`id` WHERE f.`preferida` = '1' ORDER BY `i`.`orden` ASC Limit 3")) {
     $conexion->commit();
     $conexion->close();
 } else {
